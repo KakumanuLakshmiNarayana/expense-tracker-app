7a140ac29415fd74ac8b8899f029d743f3fa665d
KakumanuLakshmiNarayana <144224826+KakumanuLakshmiNarayana@users.noreply.github.com>
2025-08-06 02:28:07 -0500

    Create App.tsx

---
+++ public/src/App.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import PersonalExpenseTracker from './PersonalExpenseTracker';
+
+export default function App() {
+  return (
+    <div className="min-h-screen bg-gray-100">
+      <PersonalExpenseTracker />
+    </div>
+  );
+}
